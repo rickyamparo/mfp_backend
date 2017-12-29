2bec819f66ef9692b4f7e72f404a94fcafdd71fd
Ricky Amparo <rickyamparo@gmail.com>
2017-12-29 14:04:36 -0700

    add user destroy all to the start of my seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
 
 User.create(name: 'testuser', email: 'test@test.com', password: 'password')
