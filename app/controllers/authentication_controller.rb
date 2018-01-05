7391686496dda74cd72ffb4608de592ba0644f6f
Ricky Amparo <rickyamparo@gmail.com>
2018-01-04 17:27:42 -0700

    change the user_id to be sent out as a string along with the auth_token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
 
     if command.success?
       user = User.find_by_email(params[:email])
-      render json: { auth_token: command.result, user_id: user.id}
+      render json: { auth_token: command.result, user_id: user.id.to_s}
     else
       render json: { error: command.errors }, status: :unauthorized
     end
