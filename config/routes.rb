4348766788c67b50aada0ab562ae7411e77a2c4f
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 19:28:57 -0700

    fix BI API routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/business_intelligence/most_visited',
-       :to => 'api/v1/business_intelligence#most_visited'
+       :to => '/business_intelligence#most_visited'
 
        get '/business_intelligence/least_visited',
-        :to => 'api/v1/business_intelligence#least_visited'
+        :to => '/business_intelligence#least_visited'
     end
   end
 end
