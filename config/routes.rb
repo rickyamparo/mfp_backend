486d79bee1e78f658b1cce65791b03ef5690ffba
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 19:27:34 -0700

    add business intelligence API routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   resources :locations
   post 'authenticate', to: 'authentication#authenticate'
+
+  namespace :api do
+    namespace :v1 do
+      get '/business_intelligence/most_visited',
+       :to => 'api/v1/business_intelligence#most_visited'
+
+       get '/business_intelligence/least_visited',
+        :to => 'api/v1/business_intelligence#least_visited'
+    end
+  end
 end
