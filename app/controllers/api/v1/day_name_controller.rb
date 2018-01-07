bea15c8644e7b26dec3531f93351a08fe4112554
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 21:36:56 -0700

    add skeleton of actions to controller

--- app/controllers/api/v1/day_name_controller.rb
+++ app/controllers/api/v1/day_name_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::DayNameController < ApplicationController
+
+  def most_popular
+    user = User.find(location_params[:user_id])
+  end
+
+  def least_popular
+    user = User.find(location_params[:user_id])
+  end
+
+  private
+
+  def day_params
+    params.permit(:user_id, :day_name)
+  end
+
 end
