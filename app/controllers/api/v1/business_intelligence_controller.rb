c3c501837355bbe03da90793c6a46e2aaeb87d09
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 20:24:44 -0700

    fixed endpoints to display information as proper JSON

--- app/controllers/api/v1/business_intelligence_controller.rb
+++ app/controllers/api/v1/business_intelligence_controller.rb
@@ -1,14 +1,20 @@
-class Api::V1::BusinessIntelligence < ApplicationController
+class Api::V1::BusinessIntelligenceController < ApplicationController
 
   def most_visited
     user = User.find(location_params[:user_id])
-    render json: Location.where(user_id: user.id).group(:longitude, :latitude).count.max
+    most_visited_arr = Location.where(user_id: user.id).group(:longitude, :latitude).count.max
+    return_value = {coordinates: {longitude: most_visited_arr[0][0], latitude: most_visited_arr[0][1]}, times_visited: most_visited_arr[1]}
+    render json: return_value
   end
 
   def least_visited
     user = User.find(location_params[:user_id])
-    render json: Location.where(user_id: user.id).group(:longitude, :latitude).count.min
+    most_visited_arr = Location.where(user_id: user.id).group(:longitude, :latitude).count.min
+    return_value = {coordinates: {longitude: most_visited_arr[0][0], latitude: most_visited_arr[0][1]}, times_visited: most_visited_arr[1]}
+    render json: return_value
   end
+
+  private
 
   def location_params
     params.permit(:user_id)
