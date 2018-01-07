0675f32d8afaea82883091ba1239ddc40e52ea8b
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 19:12:55 -0700

    create activerecord query for most_visited, based least_visited on that

--- app/controllers/api/v1/business_intelligence_controller.rb
+++ app/controllers/api/v1/business_intelligence_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::BusinessIntelligence < ApplicationController
 
   def most_visited
+    user = User.find(location_params[:user_id])
+    render json: Location.where(user_id: user.id).group(:longitude, :latitude).count.max
+  end
+
+  def least_visited
+    user = User.find(location_params[:user_id])
+    render json: Location.where(user_id: user.id).group(:longitude, :latitude).count.min
   end
 
   def location_params
