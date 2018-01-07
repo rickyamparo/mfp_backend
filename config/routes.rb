7cf02f0e41ffa6894bd1e6410117e5d480743c6b
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 20:25:15 -0700

    fixed endpoints to display information as proper JSON

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/business_intelligence/most_visited',
-       :to => '/business_intelligence#most_visited'
-
-       get '/business_intelligence/least_visited',
-        :to => '/business_intelligence#least_visited'
     end
   end
+
+  get '/api/v1/business_intelligence/most_visited/',
+  :to => 'api/v1/business_intelligence#most_visited'
+
+  get '/api/v1/business_intelligence/least_visited/',
+  :to => 'api/v1/business_intelligence#least_visited'
+
 end
