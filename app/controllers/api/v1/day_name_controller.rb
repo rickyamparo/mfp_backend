27794d84f2477d1d4294f56d7c383b240b7031e9
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 21:44:58 -0700

    add the rest of the action methods

--- app/controllers/api/v1/day_name_controller.rb
+++ app/controllers/api/v1/day_name_controller.rb
@@ -2,10 +2,16 @@ class Api::V1::DayNameController < ApplicationController
 
   def most_popular
     user = User.find(location_params[:user_id])
+    location = Location.where(user_id: user).where(day_name: location_params[:day_name]).group(:longitude, :latitude).count.max
+    return_value = {coordinates: {longitude: location[0][0], latitude: location[0][1]}, times_visited: location[1]}
+    render json: return_value
   end
 
   def least_popular
     user = User.find(location_params[:user_id])
+    location = Location.where(user_id: user).where(day_name: location_params[:day_name]).group(:longitude, :latitude).count.min
+    return_value = {coordinates: {longitude: location[0][0], latitude: location[0][1]}, times_visited: location[1]}
+    render json: return_value
   end
 
   private
