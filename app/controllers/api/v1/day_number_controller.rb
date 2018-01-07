0360b3a1302a281c4cb6058395df3dd9664c34da
Ricky Amparo <rickyamparo@gmail.com>
2018-01-06 21:54:48 -0700

    create most popular action for the day number controller

--- app/controllers/api/v1/day_number_controller.rb
+++ app/controllers/api/v1/day_number_controller.rb
@@ -1,3 +1,16 @@
 class Api::V1::DayNumberController < ApplicationController
-  
+
+  def most_popular
+    user = User.find(day_params[:user_id])
+    location = Location.where(user_id: user).where(day_number: day_params[:day_number]).group(:longitude, :latitude).count.max
+    return_value = {coordinates: {longitude: location[0][0], latitude: location[0][1]}, times_visited: location[1]}
+    render json: return_value
+  end
+
+  private
+
+  def day_params
+    params.permit(:user_id, :day_number)
+  end
+
 end
